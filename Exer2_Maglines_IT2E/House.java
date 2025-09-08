311bb31039e8de7f556ce3921e8c1887f84bc399
maglinesangelaashton-boop <maglinesangelaashton@gmail.com>
2025-09-08 22:57:43 +0800

    Update House.java

--- Exer2_Maglines_IT2E/House.java
+++ Exer2_Maglines_IT2E/House.java
@@ -1,1 +1,12 @@
+public class HouseTester {
+	
+	    public static void main(String[] args) {
+	        // Create House objects
+	        House h1 = new House("White", "3", "123 Main St", "Mr. Cruz", "200 sqm");
+	        House h2 = new House("Blue", "5", "456 Oak Ave", "Mrs. Reyes", "350 sqm");
 
+	        // Display their information
+	        h1.displayInfo();
+	        h2.displayInfo();
+	    }
+}
